0481b21788b0e3448cfee4572af48389626de63b
Myke D <gr0m17@gmail.com>
2022-01-05 04:24:52 -0600

    onEnd is working wonders. needs refactoring for tree data.

---
+++ src/datastructure.js
@@ -0,0 +1,23 @@
+//each video has an entry that describes it and it's branches.
+//branches will enable buttons.
+const videoTree = {
+  videoID: "PRS_V03",
+  videoSRC: "file",
+  branches: {
+    b1: {
+      videoID: "PRS_V04",
+      videoSRC: "file",
+      name: "Rock",
+    },
+    b2: {
+      videoID: "PRS_V08",
+      videoSRC: "file",
+      name: "Paper",
+    },
+    b3: {
+      videoID: "PRS_V12",
+      videoSRC: "file",
+      name: "Scissor",
+    },
+  },
+};
