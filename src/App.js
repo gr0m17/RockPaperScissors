b94a575048b169ee85f2074befd1f15579180430
Myke D <gr0m17@gmail.com>
2022-01-05 03:48:19 -0600

    onEnd is working wonders. needs refactoring for tree data.

--- src/App.js
+++ src/App.js
@@ -14,11 +14,72 @@ import RPS_V13 from "./video/RPS_V13.mp4";
 import RPS_V14 from "./video/RPS_V14.mp4";
 
 function App() {
+  let videoPlayer = document.getElementById("videoWindow");
+  console.log(videoPlayer);
+  const VIDEO_ARRAY = [
+    RPS_V03,
+    RPS_V03,
+    RPS_V03,
+    RPS_V04,
+    RPS_V05,
+    RPS_V06,
+    RPS_V07,
+    RPS_V08,
+    RPS_V09,
+    RPS_V09,
+    RPS_V10,
+    RPS_V11,
+    RPS_V12,
+    RPS_V13,
+    RPS_V14,
+  ];
+  let currentVideo = 0;
+  const rockHandler = () => {
+    videoPlayer = document.getElementById("videoWindow");
+    const cpuChoice = Math.floor(Math.random() * 3) + 3;
+    currentVideo = cpuChoice;
+  };
+  const paperHandler = () => {
+    videoPlayer = document.getElementById("videoWindow");
+    const cpuChoice = Math.floor(Math.random() * 3) + 7;
+    currentVideo = cpuChoice;
+  };
+  const scissorHandler = () => {
+    videoPlayer = document.getElementById("videoWindow");
+    const cpuChoice = Math.floor(Math.random() * 3) + 11;
+    currentVideo = cpuChoice;
+  };
+
+  const endedHandler = (event) => {
+    videoPlayer = document.getElementById("videoWindow");
+    videoPlayer.autoplay = true;
+    videoPlayer.src = VIDEO_ARRAY[currentVideo];
+    videoPlayer.load();
+    console.log(event);
+    currentVideo++;
+  };
+  const playHandler = () => {
+    videoPlayer = document.getElementById("videoWindow");
+    videoPlayer.play();
+  };
   return (
     <div className="App">
-      <video>
-        <source src={RPS_V09}></source>
+      <video
+        width="800"
+        id="videoWindow"
+        onEnded={endedHandler}
+        autoPlay={"autoplay"}
+      >
+        <source src={RPS_V03}></source>
       </video>
+      <div>
+        <button onClick={rockHandler}>ROCK</button>
+        <button onClick={paperHandler}>PAPER</button>
+        <button onClick={scissorHandler}>SCISSORS</button>
+      </div>
+      <div>
+        <button onClick={playHandler}>PLAY</button>
+      </div>
     </div>
   );
 }
